19bf13dde4891890897149dc5c4ef660eb296d59
ShadowPam <31468537+ShadowPam@users.noreply.github.com>
2018-02-11 19:28:29 +0100

    ImageTile class done
    done

--- Game_engine/src/game_engine/Render.java
+++ Game_engine/src/game_engine/Render.java
@@ -6,6 +6,7 @@
 package game_engine;
 
 import game_engine.gfx.Image;
+import game_engine.gfx.ImageTile;
 import java.awt.image.DataBufferInt;
 
 /**
@@ -44,16 +45,11 @@ public class Render{
     //Draws an image on screen
     public void drawImage(Image image, int offX, int offY){
     
-        int newX = 0;
-        int newY = 0;
-        int newWidth = image.getWidth();
-        int newHeight = image.getHeight();
-        
         //Don't render
-        if(offX < -newWidth){
+        if(offX < -image.getWidth()){
             return;  
         }
-        if(offY < -newHeight){
+        if(offY < -image.getHeight()){
             return;  
         }
         if(offX >= pW){
@@ -62,6 +58,11 @@ public class Render{
         if(offY >= pH){
             return;  
         }
+        
+        int newX = 0;
+        int newY = 0;
+        int newWidth = image.getWidth();
+        int newHeight = image.getHeight();
         
         //Clipping
         if(offX < 0){
@@ -83,6 +84,54 @@ public class Render{
             for(int x = newX; x < newWidth; x++){
                 
                 setPixel(x + offX, y + offY, image.getPixel()[x + y * image.getWidth()]);
+                
+            }
+            
+        }
+        
+    }
+    
+    public void drawImageTile(ImageTile image, int offX, int offY, int tileX, int tileY){
+    
+        //Don't render
+        if(offX < -image.getTileWidth()){
+            return;  
+        }
+        if(offY < -image.getTileHeight()){
+            return;  
+        }
+        if(offX >= pW){
+            return;  
+        }
+        if(offY >= pH){
+            return;  
+        }
+        
+        int newX = 0;
+        int newY = 0;
+        int newWidth = image.getTileWidth();
+        int newHeight = image.getTileHeight();
+        
+        //Clipping
+        if(offX < 0){
+            newX -= offX; 
+        }
+        if(offY < 0){
+            newY -= offY;
+        }
+        if(newWidth + offX >= pW){
+            newWidth -= newWidth + offX - pW;
+        }
+        if(newHeight + offY >= pH){
+            newHeight -= newHeight + offY - pH;
+        }
+        
+        //Render code
+        for(int y = newY; y < newHeight; y++){
+        
+            for(int x = newX; x < newWidth; x++){
+                
+                setPixel(x + offX, y + offY, image.getPixel()[(x + tileX * image.getTileWidth()) + (y + tileY * image.getTileHeight()) * image.getWidth()]);
                 
             }
             
